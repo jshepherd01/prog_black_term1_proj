a828408537647d5cdbf58ff07fe5c03dbb3e5d8b
Joe Shepherd <sudo.jshepherd01@gmail.com>
2020-12-12 14:17:56 +0000

    Create upload form, closes #1

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # Front End
 
 - Add tooltips to first forms
-- Add 'copy passcode to clipboard' button
 - Add Advanced Options dropdown
 - Send form data
 - Add additional forms
--- static/js/client.js
+++ static/js/client.js
@@ -16,13 +16,22 @@ const dropUpload = document.getElementById('drop-upload');
 const dropView = document.getElementById('drop-view');
 const frmUpload = document.getElementById('frm-upload');
 const frmView = document.getElementById('frm-view');
+const buttonUploadFile = document.getElementById('upload-file-btn');
+const inputUploadFile = document.getElementById('upload-file');
+const previewUploadFile = document.getElementById('upload-file-name');
 const toggleUploadPriv = document.getElementById('upload-priv-tog');
 const checkUploadPriv = document.getElementById('upload-priv');
 const containerUploadViewPass = document.getElementById('upload-view-pass-container');
 const inputUploadEditPass = document.getElementById('upload-edit-pass');
 const inputUploadViewPass = document.getElementById('upload-view-pass');
 const refreshUploadEditPass = document.getElementById('upload-edit-pass-refresh');
+const copyUploadEditPass = document.getElementById('upload-edit-pass-copy');
 const refreshUploadViewPass = document.getElementById('upload-view-pass-refresh');
+const copyUploadViewPass = document.getElementById('upload-view-pass-copy');
+const buttonCollapsibleUpload = document.getElementById('clps-btn-upload');
+const formCollapsibleUpload = document.getElementById('clps-frm-upload');
+const toggleUploadNsfw = document.getElementById('upload-nsfw-tog');
+const inputUploadNsfw = document.getElementById('upload-nsfw');
 
 /* add event handlers */
 dropUpload.addEventListener('click', (event) => {
@@ -45,26 +54,97 @@ dropView.addEventListener('click', (event) => {
     }
 })
 
+buttonUploadFile.addEventListener('click', (event) => {
+    inputUploadFile.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }))
+})
+
+inputUploadFile.addEventListener('change', (event) => {
+    let pathArray = inputUploadFile.value.split("\\")
+    previewUploadFile.innerText = pathArray[pathArray.length - 1];
+})
+
 toggleUploadPriv.addEventListener('click', (event) => {
-    if (checkUploadPriv.checked) {
-        toggleUploadPriv.innerText = "NO";
-        checkUploadPriv.checked = false;
+    checkUploadPriv.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }))
+})
+
+checkUploadPriv.addEventListener('change', (event) => {
+    if (!checkUploadPriv.checked) {
+        toggleUploadPriv.firstElementChild.innerText = "close";
         containerUploadViewPass.classList.add('hide');
     } else {
-        toggleUploadPriv.innerText = "YES";
-        checkUploadPriv.checked = true;
+        toggleUploadPriv.firstElementChild.innerText = "check";
         containerUploadViewPass.classList.remove('hide');
     }
 })
 
 refreshUploadEditPass.addEventListener('click', (event) => {
-    event.preventDefault();
     inputUploadEditPass.value = genPass();
 })
 
+inputUploadEditPass.addEventListener('click', (event) => {
+    copyUploadEditPass.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }))
+})
+
+copyUploadEditPass.addEventListener('click', (event) => {
+    inputUploadEditPass.select();
+    inputUploadEditPass.setSelectionRange(0,24);
+    document.execCommand('copy');
+})
+
 refreshUploadViewPass.addEventListener('click', (event) => {
-    event.preventDefault();
     inputUploadViewPass.value = genPass();
+})
+
+inputUploadViewPass.addEventListener('click', (event) => {
+    copyUploadViewPass.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }))
+})
+
+copyUploadViewPass.addEventListener('click', (event) => {
+    inputUploadViewPass.select();
+    inputUploadViewPass.setSelectionRange(0,24);
+    document.execCommand('copy');
+})
+
+buttonCollapsibleUpload.addEventListener('click', (event) => {
+    if (!formCollapsibleUpload.classList.contains('hide')) {
+        buttonCollapsibleUpload.innerText = "Show More Options";
+        formCollapsibleUpload.classList.add('hide');
+    } else {
+        buttonCollapsibleUpload.innerText = "Show Fewer Options";
+        formCollapsibleUpload.classList.remove('hide');
+    }
+})
+
+toggleUploadNsfw.addEventListener('click', (event) => {
+    inputUploadNsfw.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    }))
+})
+
+inputUploadNsfw.addEventListener('click', (event) => {
+    if (!inputUploadNsfw.checked) {
+        toggleUploadNsfw.firstElementChild.innerText = "close";
+    } else {
+        toggleUploadNsfw.firstElementChild.innerText = "check";
+    }
 })
 
 /* load event */
